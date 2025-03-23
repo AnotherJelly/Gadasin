ae37a2ec642b7a45f506030f9fcaf485afce6b74
AnotherJelly <bespalov-maksim@list.ru>
2025-03-23 18:39:25 +0300

    Update src files

--- src/App.js
+++ src/App.js
@@ -5,30 +5,24 @@ const settings = {
         url: "http://localhost:5000/api/location",
         objectId: "123",
     },
-    titleRadio: "Режим работы",
+    titleRadio: "Таблицы",
     titleInput: "Фильтр даты",
     titleList: "Точки",
     inputDate: [
         { id: "date_from", desc: "Дата от" },
         { id: "date_to", desc: "Дата до" }
     ],
-    radioModes: [
-        { id: "track", desc: "Отслеживать последнее" },
-        { id: "history", desc: "История местоположений" },
+    tablesDB: [
+        { id: "sensors", desc: "Таблицы БД" },
     ],
-    promts: {
-        promtList: { title: "Точки", desc: "Описание точек" },
-        promtRadio: { title: "Режим работы", desc: "Описание режимов работы" },
-        promtInput: { title: "Фильтр даты", desc: "Описание фильтра даты" }
-    },
     img: { width: 993, height: 656 }
 }
 
-function MenuDateInput({ title, inputDate, openModal, promt, onChange }) {
+function MenuDateInput({ title, inputDate, onChange }) {
 
     return(
         <fieldset className="menu-fieldset">
-            <legend>{title} <span onClick={() => openModal(promt)}>?</span></legend>
+            <legend>{title}</legend>
 
             {inputDate.map((date) => (
                 <div key={date.id} className="menu-input">
@@ -41,16 +35,15 @@ function MenuDateInput({ title, inputDate, openModal, promt, onChange }) {
     );    
 }
 
-function MenuRadio({ title, radioModes, openModal, promt }) {
+function MenuDB({ title, tablesDB, openModal }) {
 
     return(
         <fieldset className="menu-fieldset">
-            <legend>{title} <span onClick={() => openModal(promt)}>?</span></legend>
+            <legend>{title}</legend>
 
-            {radioModes.map((mode) => (
-                <div key={mode.id} className="menu-radio">
-                    <input type="radio" id={mode.id} name="menu-radio" value={mode.id}  />
-                    <label htmlFor={mode.id}>{mode.desc}</label>
+            {tablesDB.map((mode) => (
+                <div key={mode.id} className="menu-db">
+                    <button type="button" onClick={() => openModal()}>{mode.desc}</button>
                 </div>
             ))}
 
@@ -62,17 +55,14 @@ function MenuFilter({ openModal, onInputDate }) {
 
     return(
         <>
-            <MenuRadio
+            <MenuDB
                 title={settings.titleRadio}
-                radioModes={settings.radioModes}
+                tablesDB={settings.tablesDB}
                 openModal={openModal}
-                promt={settings.promts.promtRadio}
             />
             <MenuDateInput
                 title={settings.titleInput}
                 inputDate={settings.inputDate}
-                openModal={openModal}
-                promt={settings.promts.promtInput}
                 onChange={onInputDate}
             />
         </>
@@ -94,11 +84,11 @@ function MenuElement({ index, point, active, onClick }) {
     ); 
 }
 
-function MenuList({ title, points, activeIndexes, toggleActive, openModal, promt }) {
+function MenuList({ title, points, activeIndexes, toggleActive }) {
 
     return(
         <fieldset className="menu-fieldset menu-fieldset__points">
-            <legend>{title} <span onClick={() => openModal(promt)}>?</span></legend>
+            <legend>{title}</legend>
             <div className="menu-list">
                 {(points?.length === 0) ? 
                     <div>Точки отсутствуют</div> :
@@ -132,8 +122,6 @@ function SidebarMenu({ isMenuOpen, setIsMenuOpen, points, activeIndexes, toggleA
                     points={points} 
                     activeIndexes={activeIndexes} 
                     toggleActive={toggleActive}
-                    openModal={openModal}
-                    promt={settings.promts.promtList}
                 />
             </div>
         </div>
@@ -141,6 +129,33 @@ function SidebarMenu({ isMenuOpen, setIsMenuOpen, points, activeIndexes, toggleA
 }
 
 function Model({ points, activeIndexes }) {
+
+    const renderRoute = () => {
+        if (activeIndexes.length < 2) return null;
+    
+        const sortedIndexes = [...activeIndexes].sort((a, b) => a - b);
+    
+        const routePoints = sortedIndexes.map(index => ({
+            x: (points[index].x / settings.img.width) * 100,
+            y: (points[index].y / settings.img.height) * 100
+        }));
+    
+        const pathData = routePoints.map((point, i) =>
+            i === 0 ? `M ${point.x} ${100 - point.y}` : `L ${point.x} ${100 - point.y}`
+        ).join(' ');
+    
+        return (
+            <svg className="route-svg" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <defs>
+                    <marker id="arrow" markerWidth="3" markerHeight="3" refX="-4" refY="1.5" orient="auto" markerUnits="strokeWidth">
+                        <path d="M3,0 L0,1.5 L3,3" fill="black" />
+                    </marker>
+                </defs>
+    
+                <path d={pathData} stroke="black" strokeWidth="0.5" fill="none" markerStart="url(#arrow)" />
+            </svg>
+        );
+    };
 
     return (
         <div className="main-content">
@@ -164,26 +179,137 @@ function Model({ points, activeIndexes }) {
                         </button>
                     )
                 })}
+
+                {renderRoute()}
             </div>
         </div>
     );
 }
 
-function DescModal({ closeModal, isModalOpen, promt }) {
+function DescModal({ closeModal, isModalOpen }) {
+    const [activeTab, setActiveTab] = useState("sensor1");
+    const [points, setPoints] = useState([]);
+
+    const fetchDataPoints = () => {
+        let url = `${settings.api.url}?object_id=${settings.api.objectId}&to_time=${new Date().toISOString()}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setPoints(data.locations))
+            .catch((err) => console.error("Ошибка загрузки:", err));
+    };
+
+    const tablePoints = () => {
+        fetchDataPoints();
+        handleTabClick("sensor2");
+    }
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
+
     return (
         <div className={`modal-overlay ${isModalOpen ? "open" : ""}`}>
             <div className="modal-background" onClick={closeModal}></div>
             <div className="modal-content">
                 <div className="modal-content__title">
-                    <span>{promt.title}</span>
+                    <span>Таблицы БД</span>
                     <button onClick={closeModal}>
                         <i className="fas fa-times"></i>
                     </button>
                 </div>
-                <div className="modal-content__text">
-                    {promt.desc}
+
+                <div className="modal-content__tabs">
+                    <button
+                        onClick={() => handleTabClick("sensor1")}
+                        className={activeTab === "sensor1" ? "active" : ""}
+                    >
+                        Свойства датчиков
+                    </button>
+                    <button
+                        onClick={() => tablePoints()}
+                        className={activeTab === "sensor2" ? "active" : ""}
+                    >
+                        История местоположений
+                    </button>
                 </div>
 
+                <div className="modal-content__table">
+                    {activeTab === "sensor1" && (
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Название</th>
+                                    <th>Модель</th>
+                                    <th>Комплектация</th>
+                                    <th>Серийник</th>
+                                    <th>Mac-адрес</th>
+                                    <th>Дата эксплуатации</th>
+                                    <th>Списан</th>
+                                    <th>Гарантийная информация</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Датчик температуры</td>
+                                    <td>DT-100</td>
+                                    <td>Термодатчик + крепеж</td>
+                                    <td>SN123456</td>
+                                    <td>00:1A:2B:3C:4D:5E</td>
+                                    <td>2023-05-10</td>
+                                    <td>Нет</td>
+                                    <td>Гарантия до 2026-05-10</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Датчик влажности</td>
+                                    <td>DH-200</td>
+                                    <td>Датчик + модуль связи</td>
+                                    <td>SN654321</td>
+                                    <td>00:1B:3D:5F:7A:9C</td>
+                                    <td>2022-11-23</td>
+                                    <td>Нет</td>
+                                    <td>Гарантия до 2025-11-23</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Датчик давления</td>
+                                    <td>DP-300</td>
+                                    <td>Датчик + интерфейсный кабель</td>
+                                    <td>SN789012</td>
+                                    <td>00:2C:4E:6F:8B:AD</td>
+                                    <td>2021-08-15</td>
+                                    <td>Да</td>
+                                    <td>Гарантия истекла</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    )}
+
+                    {activeTab === "sensor2" && (
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>x</th>
+                                    <th>y</th>
+                                    <th>Время</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {points?.map((point, index) => (
+                                    <tr key={index}>
+                                        <td>{index}</td>
+                                        <td>{point.x}</td>
+                                        <td>{point.y}</td>
+                                        <td>{point.timestamp}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
+                </div>
             </div>
         </div>
     );
@@ -194,7 +320,6 @@ export function App() {
     const [dateFrom, setDateFrom] = useState(null);
     const [dateTo, setDateTo] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [promt, setPromt] = useState({ title: "", desc: "" });
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [activeIndexes, setActiveIndexes] = useState([]);
 
@@ -203,6 +328,8 @@ export function App() {
 
         // Корректировка временной зоны
         date.setHours(date.getHours() + 3);
+
+        console.log(date);
     
         if (isNaN(date.getTime())) {
             console.warn("Invalid date input");
@@ -213,7 +340,7 @@ export function App() {
             if (type === settings.inputDate[1].id) setDateTo(date);
         }
     };
-
+    
     const fetchData = () => {
         let url = `${settings.api.url}?object_id=${settings.api.objectId}`;
         if (dateFrom) {
@@ -235,8 +362,7 @@ export function App() {
         return () => clearInterval(intervalId);
     }, [dateFrom, dateTo]);
   
-    const openModalWithPromt = (promt) => {
-        setPromt(promt);
+    const openModal = () => {
         setIsModalOpen(true);
     };
   
@@ -248,18 +374,21 @@ export function App() {
   
     return (
         <div className="App">
-            <DescModal closeModal={() => setIsModalOpen(false)} isModalOpen={isModalOpen} promt={promt} />
-            <div className={`container ${isMenuOpen ? "menu-open" : ""}`}>
-            <Model points={points} activeIndexes={activeIndexes} />
-            <SidebarMenu
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={setIsMenuOpen}
-                points={points}
-                activeIndexes={activeIndexes}
-                toggleActive={toggleActive}
-                openModal={openModalWithPromt}
-                onInputDate={onInputDate}
+            <DescModal 
+                closeModal={() => setIsModalOpen(false)} 
+                isModalOpen={isModalOpen}
             />
+            <div className={`container ${isMenuOpen ? "menu-open" : ""}`}>
+                <Model points={points} activeIndexes={activeIndexes} />
+                <SidebarMenu
+                    isMenuOpen={isMenuOpen}
+                    setIsMenuOpen={setIsMenuOpen}
+                    points={points}
+                    activeIndexes={activeIndexes}
+                    toggleActive={toggleActive}
+                    openModal={openModal}
+                    onInputDate={onInputDate}
+                />
             </div>
         </div>
     );
